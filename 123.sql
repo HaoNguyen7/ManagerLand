e7d7b93c3b26b360c091034f2e9fe269688c175b
blossomvn1 <55186741+blossomvn1@users.noreply.github.com>
2020-12-07 16:34:44 +0700

    add database user

---
+++ 123.sql
@@ -0,0 +1,59 @@
+CREATE TABLE account
+(
+	username VARCHAR(30),
+	password VARCHAR(30),
+	typeAcc bit
+)
+
+ALTER TABLE account
+ALTER COLUMN typeAcc INT
+
+
+INSERT INTO dbo.account
+(
+    username,
+    password,
+    typeAcc
+)
+VALUES
+(   'a',  -- username - varchar(30)
+    '1',  -- password - varchar(30)
+    1 -- typeAcc - bit
+    )
+	INSERT INTO dbo.account
+	(
+	    username,
+	    password,
+	    typeAcc
+	)
+	VALUES
+	(   'b', -- username - varchar(30)
+	    '1', -- password - varchar(30)
+	    2   -- typeAcc - int
+	    )
+INSERT INTO dbo.account
+(
+    username,
+    password,
+    typeAcc
+)
+VALUES
+(   'c', -- username - varchar(30)
+    '1', -- password - varchar(30)
+    3   -- typeAcc - int
+    )
+CREATE PROC USP_Login
+@userName nvarchar(100), @passWord nvarchar(100)
+AS
+BEGIN 
+	SELECT * FROM dbo.account WHERE username=@userName AND password=@passWord
+END 
+
+
+SELECT * FROM dbo.account
+
+SELECT * FROM dbo.account WHERE username='a'
+EXEC USP_Login 'a','1'
+ALTER TABLE dbo.account
+
+
